e5ab5fb25c62e58cff7cc7508bea1d610a1cdaeb
antsol20 <antsol20@hotmail.com>
2021-07-06 15:20:24 +0100

    update

--- btc.py
+++ btc.py
@@ -1,27 +1,38 @@
 import pandas as pd
 import datetime as dt
+import requests
 
-trade_hour = 19  # hourly data - pick hour of buy sell e.g. 19 = 19:00
-from_date = dt.datetime(2021,1,1)  # from which date to run the data
+with open("apikey.txt", "r") as keyfile:
+    apikey = keyfile.readline()
 
-df = pd.read_csv('data.csv', parse_dates=['Date'])
-df = df[df.Date > from_date]
-df = df[df.Date.dt.hour == trade_hour]
-df.reset_index(drop=True, inplace=True)
+start_str = input("Enter start date in YYYYMMDD :")
+end_str = input("Enter end date in YYYYMMDD :")
+
+datefmt_in = '%Y%m%d'
+datefmt = '%Y-%m-%dT%H:%M:%SZ'
+url = 'https://api.nomics.com/v1/exchange-rates/history'
+start_dt = dt.datetime.strptime(start_str, datefmt_in)
+end_dt = dt.datetime.strptime(end_str, datefmt_in)
+
+params = {'key': apikey, 'currency': 'BTC', 'start': start_dt.strftime(datefmt), 'end': end_dt.strftime(datefmt)}
+response = requests.get(url=url, params=params)
+json = response.json()
+
+df = pd.DataFrame.from_dict(json)
 
 rows = df.shape[0]
-
 prices = []
-pair = [0,0]
+pair = [0, 0]
 
 for i in range(rows):
-    if df.at[i,'Date'].weekday() == 4:
-        pair[0] = float(df.at[i,'Close'])
+    stamp_dt = dt.datetime.strptime(df.at[i, 'timestamp'], datefmt)
+    if stamp_dt.weekday() == 4:
+        pair[0] = float(df.at[i, 'rate'])
 
-    if df.at[i,'Date'].weekday() == 0:
+    if stamp_dt.weekday() == 0:
         if pair[0] == 0:
             continue
-        pair[1] = float(df.at[i,'Close'])
+        pair[1] = float(df.at[i, 'rate'])
         prices.append(pair.copy())
 
 
@@ -32,4 +43,4 @@ for item in prices:
     cash = cash * (item[1]/item[0])
     print(f"End Cash {cash:.4f}")
 
-print(f"{cash:.4f}")
+print(f"Total Return : cd {cash:.4f}")
